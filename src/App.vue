93c910baa3489762612aee807ca69db65e69816d
superwizardclass <wizardclassio@gmail.com>
2023-10-19 23:12:39 -0700

    Fleshing out future components.

--- src/App.vue
+++ src/App.vue
@@ -3,9 +3,21 @@
 </script>
 
 <template>
-<div class="bg-slate-500 w-full min-h-screen">
-<p class="text-xl">Hello</p>
-</div>
+  <div class="container bg-slate-500 w-full min-h-screen">
+    <header class="flex justify-center items-center h-24 bg-emerald-500 font-permanent-marker text-3xl">
+      <h1 class="">s t a c k</h1>
+    </header>
+    <main class="px-2 py-2 h-72">
+      <div class="flex bg-white h-full justify-around">
+        <div class="basis-1/4">value</div>
+        <div class="flex-1">suit</div>
+        <div class="basis-1/4">value</div>
+      </div>
+    </main>
+    <footer>
+
+    </footer>
+  </div>
 </template>
 
 <style scoped></style>

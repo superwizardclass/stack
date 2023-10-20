5ee5f378acf2049f08a1e73d612144ea82f1202b
superwizardclass <wizardclassio@gmail.com>
2023-10-19 23:11:46 -0700

    Added permanent marker font for title.

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,9 @@ module.exports = {
     "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
+    fontFamily: {
+      'permanent-marker': ['Permanent Marker', 'cursive']
+    },
     extend: {},
   },
   plugins: [],
